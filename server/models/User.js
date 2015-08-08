15b9e3caba4e524743a17e085734c1e7d36f2ccf
michaelfortuna <m@ninjastack.com>
2015-08-08 15:02:37 -0400

    added sample routing modules

--- server/models/User.js
+++ server/models/User.js
@@ -1,22 +1,22 @@
-// Mongoose 
+// Mongoose
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-// bcrypt 
+// bcrypt
 var bcrypt = require('bcrypt');
 
-var User = new Schema({ 
+var User = new Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true }
 }, {collection: 'User'});
 
-User.methods.validPassword(function(password, callback) {
-  bcrypt.compare(password, this.password, function(err, res) {
-    if(res) {
-      callback(null, this);
-    } else {
-      callback(true, this);
-    }
-  });
-})
+// User.methods.validPassword(function(password, callback) {
+//   bcrypt.compare(password, this.password, function(err, res) {
+//     if(res) {
+//       callback(null, this);
+//     } else {
+//       callback(true, this);
+//     }
+//   });
+// })
 
 mongoose.model('User', User);

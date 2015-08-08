4072b63f6468c61575ac0db91406159e26ec0df4
David Kim <davidk@halopowered.com>
2015-08-08 14:32:20 -0400

    Added device schema and route

---
+++ server/models/Device.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var DeviceSchema = new Schema({
+  owner:                {type: Schema.Types.ObjectId, ref: 'User'},
+  dateCreated:          {type: Date, default: Date.now},
+  data: {},
+  filters: [{
+    _id: false,
+    variableA: {type: String},
+    variableB: {type: String},
+    operator: {type: String},
+  }],
+  API: [{
+    _id: false,
+    type: {type:String},
+    details: {},
+  }],
+
+
+}, {collection: 'Device'});
+
+mongoose.model('Device', DeviceSchema);

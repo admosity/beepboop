4302bc1f5967adeabe3779233b6d58a6a5d0205f
michaelfortuna <m@ninjastack.com>
2015-08-08 21:12:36 -0400

    i got that good shit for adam

---
+++ client/js/states/UserStates.js
@@ -0,0 +1,68 @@
+// modify folders in client/modules directory
+// sample directory for this prefix would be client/modules/sample/
+var modulePrefix = '/modules/user/';
+var viewsPrefix = modulePrefix + 'views/';
+var partialsPrefix = modulePrefix + 'partials/';
+
+module.exports = [
+  {
+    name: 'dashboard',
+    state: {
+      url: '/dashboard',
+      views: {
+        // sidebar: {
+        //   templateUrl: partialsPrefix + 'sidebar.html',
+        // },
+        content: {
+          templateUrl: viewsPrefix + 'dashboard.html',
+          // controller: 'LoginCtrl',
+        },
+      }
+    }
+  },
+  {
+    name: 'new-device',
+    state: {
+      url: '/new-device',
+      views: {
+        // sidebar: {
+        //   templateUrl: partialsPrefix + 'sidebar.html',
+        // },
+        content: {
+          templateUrl: viewsPrefix + 'new-device.html',
+          // controller: 'LoginCtrl',
+        },
+      }
+    }
+  },
+  {
+    name: 'edit-device',
+    state: {
+      url: '/edit-device',
+      views: {
+        // sidebar: {
+        //   templateUrl: partialsPrefix + 'sidebar.html',
+        // },
+        content: {
+          templateUrl: viewsPrefix + 'edit-device.html',
+          // controller: 'LoginCtrl',
+        },
+      }
+    }
+  },
+  {
+    name: 'edit-widget',
+    state: {
+      url: '/edit-widget',
+      views: {
+        // sidebar: {
+        //   templateUrl: partialsPrefix + 'sidebar.html',
+        // },
+        content: {
+          templateUrl: viewsPrefix + 'edit-widget.html',
+          // controller: 'LoginCtrl',
+        },
+      }
+    }
+  }
+]

988aef2c9ff0597ba3a87fbc52508ad5eaa16a0e
Adam Lee <adam@ninjastack.com>
2015-08-09 12:58:03 -0400

    updated demo

--- client/js/controllers/DemoCtrl.js
+++ client/js/controllers/DemoCtrl.js
@@ -3,4 +3,7 @@ module.controller('DemoCtrl', function($scope, $http) {
   $scope.activateLink = function(link) {
     $http.get(link);
   };
+  $scope.encode = function(link) {
+    return encodeURIComponent(link);
+  };
 });

1fd23e3269ca76a5878384f0e65b735bf9dfc44c
David Kim <davidk@halopowered.com>
2015-08-08 23:34:49 -0400

    another firmware fuck

--- exampleFirmware/esp8266_read/init.lua
+++ exampleFirmware/esp8266_read/init.lua
@@ -10,6 +10,54 @@ toggle = 1
 connected = 0
 count = 0
 
+gpio.mode(3,gpio.OUTPUT)
+gpio.write(3, gpio.LOW)
+
+function updateState(state)
+    if state == "0" then
+        gpio.write(3, gpio.LOW)
+    else
+        gpio.write(3, gpio.HIGH)
+    end
+end
+
+function readBeepBoop(status)
+    print("Update host")
+    conn=net.createConnection(net.TCP, 0)
+    
+    conn:on("receive", function(conn, payload)
+        conn:close()
+        pos = string.find(payload,'\r\n\r\n')
+        if pos ~= nil then
+            strjson = string.sub(payload, pos)
+            -- gpio.write(2, gpio.LOW)
+            json = cjson.decode(strjson)
+            if json['payload'] ~= nil then
+                updateState(json['payload'])
+            end
+        end
+        
+    end )
+    
+    --/api/devices/:id/payload?write=key&read=key&payload=whateve
+
+    conn:on("connection", function(conn, payload) 
+        print('\nSending to host') 
+        conn:send("GET /api/devices/"
+        ..deviceId
+        .."/payload?read="..readKey
+        .." HTTP/1.1\r\n"
+        .."Host: beepboop.herokuapp.com\r\n"
+        .."Connection: close\r\nAccept: */*\r\n\r\n")
+    end)
+    
+    conn:on("disconnection",function(conn) 
+      conn:close()
+    end)
+    
+    conn:connect(80,"beepboop.herokuapp.com")
+end
+
 tmr.alarm(0, 1000, 1, function()
     if connected == 0 then
         if toggle == 1 then
@@ -45,34 +93,3 @@ tmr.alarm(0, 1000, 1, function()
     end
     
 end)
-
-function readBeepBoop(status)
-    print("Update host")
-    conn=net.createConnection(net.TCP, 0)
-    
-    conn:on("receive", function(conn, payload)
-        tmr.stop(1)
-        print(payload)
-        conn:close()
-        gpio.write(2, gpio.LOW)
-    end )
-    
-    --/api/devices/:id/payload?write=key&read=key&payload=whateve
-
-    conn:on("connection", function(conn, payload) 
-        print('\nSending to host') 
-        conn:send("GET /api/devices/"
-        ..deviceId
-        .."/payload?read="..readKey
-        .." HTTP/1.1\r\n"
-        .."Host: beepboop.herokuapp.com\r\n"
-        .."Connection: close\r\nAccept: */*\r\n\r\n")
-    end)
-    
-    conn:on("disconnection",function(conn) 
-      conn:close()
-    end)
-    
-    conn:connect(80,"beepboop.herokuapp.com")
-end
--- sendNetwork(1);
